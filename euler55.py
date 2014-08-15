246cc2fef7b5ed601d03ebb7ce81550c4d5fe780
Kile Deal <aurenos12@gmail.com>
2014-08-15 18:54:33 +0000

    Solved Problem 55

---
+++ euler55.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+def is_lychrel(x):
+    result = x
+    for _ in xrange(51):
+        result = result + int(str(result)[::-1])
+        if int(str(result)) == int(str(result)[::-1]):
+            print x, result
+            return False
+    
+    return True
+
+lychrel_ct = 0
+    
+for i in xrange(10000):
+    lychrel_ct += 1 if is_lychrel(i) else 0
+    
+print lychrel_ct
